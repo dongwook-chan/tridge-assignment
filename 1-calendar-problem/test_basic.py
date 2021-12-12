db50b3597b6da8d54882ebf2e87ca8f6de0e1927
dongwook-chan <dongwook.chan@gmail.com>
2021-12-13 02:33:58 +0900

    Split test for assignment #1
    1. change name of test_basic to test_by_century
      - test dow counter for each century
    
    2. add test_by_month
      - test dow for each month

--- 1-calendar-problem/test_basic.py
+++ 1-calendar-problem/test_basic.py
@@ -1,19 +1,71 @@
+from typing import List
+
 from dow_counter import DowCounter, StdLibDowCounter
 
 
-def test_answer() -> None:
+error_template = ("incorrect solution of {class_name} for "
+                  "{century}th century: {solution}\n"
+                  "correct answer: {answer}\n")
+
+
+def counters_to_test(
+        century: int, counter_to_ignore: List[str] = []) -> List[object]:
+    counter_to_ignore.append('StdLibDowCounter')
+    instances = []
+
+    for subclass in DowCounter.__subclasses__():
+        if subclass.__name__ in counter_to_ignore:
+            continue
+        instance = subclass(century)
+        instances.append(instance)
+
+    return instances
+
+
+def test_by_month() -> None:
+    year_lower = 1
+    year_upper = 9999
+    century_lower = year_lower // 100 + 1
+
+    accurate_counter = StdLibDowCounter(century_lower)
+
+    for counter in counters_to_test(
+            century_lower, counter_to_ignore=['TabularDowCounter']):
+        for year in range(year_lower, year_upper + 1):
+            century = year // 100 + 1
+            for month in range(1, 13):
+                answer = accurate_counter.get_day_of_week(
+                    year=year, month=month, day=1)
+                solution = counter.get_day_of_week(
+                    year=year, month=month, day=1)
+
+                error_template = (
+                    "incorrect solution of {class_name} for "
+                    "{century}th century {year}th year {month}th month"
+                    ": {solution}\ncorrect answer: {answer}\n")
+
+                error_message = error_template.format(
+                    class_name=counter.__class__.__name__, century=century,
+                    year=year, month=month, solution=solution, answer=answer)
+
+                assert answer == solution, error_message
+
+
+def test_by_century() -> None:
     for century in range(1, 100):
-        std_lib_dow_counter = StdLibDowCounter(century)
-        answer = std_lib_dow_counter.count_dow_of_first_day_in_month()
+        accurate_counter = StdLibDowCounter(century)
+        answer = accurate_counter.count_dow_of_first_day_in_month()
 
-        for subclass in DowCounter.__subclasses__():
-            if subclass.__name__ == 'StdLibDowCounter':
-                continue
-            instance = subclass(century)
-            solution = instance.count_dow_of_first_day_in_month()
+        for counter in counters_to_test(century):
+            solution = counter.count_dow_of_first_day_in_month()
 
-            err = (f"incorrect solution of {subclass.__name__} for "
-                   f"{century}th century: {solution}\n"
-                   f"correct answer: {answer}\n")
+            error_template = (
+                "incorrect solution of {class_name} for "
+                "{century}th century: {solution}\n"
+                "correct answer: {answer}\n")
 
-            assert answer == solution, err
+            error_message = error_template.format(
+                class_name=counter.__class__.__name__, century=century,
+                solution=solution, answer=answer)
+
+            assert answer == solution, error_message
